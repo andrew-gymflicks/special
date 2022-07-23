cdd1a76346cd16fb803bdba96d75afa32449e344
Aarush S <main@gymflicks.com>
2022-07-22 21:46:06 -0500

    Initial Commit

---
+++ GymFlicks/GymFlicksApp.swift
@@ -0,0 +1,17 @@
+//
+//  GymFlicksApp.swift
+//  GymFlicks
+//
+//  Created by Anil Sawhney on 7/22/22.
+//
+
+import SwiftUI
+
+@main
+struct GymFlicksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
